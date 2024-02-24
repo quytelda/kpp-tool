609c8b4f0fb32a98fedead06b5787c0b5c704b7d
Quytelda Kahja <quytelda@tamalin.org>
2024-02-24 00:43:51 -0800

    Add 'Options' record and 'Operation' sum type.

--- app/Main.hs
+++ app/Main.hs
@@ -10,11 +10,43 @@ import           Preset
 versioner :: Parser (a -> a)
 versioner = simpleVersioner $ showVersion version
 
-parser :: ParserInfo a
-parser = info (empty <**> helper <**> versioner)
-         (  fullDesc
-         <> header "kpp-tool - a CLI tool for inspecting and editing KPP files"
-         )
+-- | Runtime Operations
+data Operation = OpShow -- ^ Display a summary of information about a
+                        -- preset.
+               deriving (Show)
+
+showOption :: Parser Operation
+showOption = flag' OpShow
+  (  long "show"
+  <> short 's'
+  <> help "Display a summary of preset settings"
+  )
+
+operation :: Parser Operation
+operation = showOption
+
+inputFile :: Parser FilePath
+inputFile = argument str (metavar "FILE")
+
+-- | 'Options' represents a selection of runtime configuration
+-- options and arguments.
+data Options = Options
+  { optOperation :: Operation
+  , optInput     :: FilePath
+  } deriving (Show)
+
+options :: Parser Options
+options = Options
+  <$> operation
+  <*> inputFile
+  <**> helper
+  <**> versioner
+
+parser :: ParserInfo Options
+parser = info options
+  ( fullDesc
+  <> header "kpp-tool - a CLI tool for inspecting and editing KPP files"
+  )
 
 main :: IO ()
 main = undefined

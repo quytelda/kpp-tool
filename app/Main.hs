dc7a28af99f297101993c1f94a5ae53cfbb6668a
Quytelda Kahja <quytelda@tamalin.org>
2024-02-23 22:50:57 -0800

    Add a simple option parser with a '--help' option.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main (main) where
 
-import Preset ()
+import           Options.Applicative
+
+import           Preset              ()
+
+parser :: ParserInfo a
+parser = info (empty <**> helper)
+         (  fullDesc
+         <> header "kpp-tool - a CLI tool for inspecting and editing KPP files"
+         )
 
 main :: IO ()
 main = undefined

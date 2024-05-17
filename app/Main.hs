ab60eaeb5ad030ac247501982a68ed1342e39752
Quytelda Kahja <quytelda@tamalin.org>
2024-05-17 07:30:39 -0700

    Define the Action type.
    An Action is a composable IO action that takes a preset as an input
    and returns a (possibly) modified preset. Most command line switches
    should correspond to an Action. Thus, the list of switches passed to
    the program corresponds to a sequence of Actions that will be applied
    to a Preset and whose output can be written to file.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
-module Main (main) where
+module Main where
+
+import           Data.Functor
+import           Prettyprinter
+import           Prettyprinter.Render.Text
+
+import           Preset
+
+-- | An `Action` is function that (possibly) modifies a preset in the
+-- IO monad. For example `showPreset` prints a preset description to
+-- the console while passing the preset through unchanged. `setName`
+-- updates the name of the preset.
+--
+-- An action is just a Kleisli arrow of the form @Preset -> IO
+-- Preset@, so they are composable using `>>=` and `>=>`. Most command
+-- line switches correspond to a particular action, so the full
+-- command line arguments passed to the program represents a chain of
+-- sequenced `Action`s.
+type Action = Preset -> IO Preset
+
+-- | Discard is a data sink which discards any input sent to it.
+discard :: Applicative f => a -> f ()
+discard = const $ pure ()
+
+showPreset :: Action
+showPreset preset = putDoc (pretty preset) *> putChar '\n' $> preset
 
 main :: IO ()
 main = putStrLn "Not implemented yet."

e9aa902e403f5c7c68894881c29ca4ea1572c71f
Quytelda Kahja <quytelda@tamalin.org>
2024-06-04 23:53:10 -0700

    Simplify unit tests and improve formatting.

--- test/Spec.hs
+++ test/Spec.hs
@@ -14,31 +14,28 @@ main = hspec specPreset
 
 specPreset :: Spec
 specPreset = describe "Preset" $ do
-    before (BL.readFile "kpp/basic-shape-grainy.kpp") $ do
-      specify "can parse preset version" $ \content ->
-        let Preset{..} = decode content
-        in presetVersion == "5.0"
-      specify "can parse preset names" $ \content ->
-        let Preset{..} = decode content
-        in presetName == "e) Basic Shape Grainy"
-      specify "can parse preset parameters" $ \content -> do
-        let Preset{..} = decode content
+    before (decode <$> BL.readFile "kpp/basic-shape-grainy.kpp") $ do
+      specify "can parse preset version" $ \Preset{..} ->
+        presetVersion `shouldBe` "5.0"
 
+      specify "can parse preset names" $ \Preset{..} ->
+        presetName `shouldBe` "e) Basic Shape Grainy"
+
+      specify "can parse preset parameters" $ \Preset{..} -> do
         length presetParams `shouldBe` 225
 
         Map.lookup "ColorSource/Type" presetParams `shouldBe` Just (String   "plain")
         Map.lookup "EraserMode"       presetParams `shouldBe` Just (Internal "false")
-      specify "can parse embedded resources" $ \content -> do
-        let Preset{..} = decode content
 
+      specify "can parse embedded resources" $ \Preset{..} -> do
         -- The example has 2 embedded resources: a brush and a pattern.
         length embeddedResources `shouldBe` 2
 
         embeddedResources `shouldSatisfy` Map.member "egg"
         embeddedResources `shouldSatisfy` Map.member "hourglass.png"
-      specify "can parse preset icons" $ \content -> do
-        let Preset{..} = decode content
-            ihdrChunk = head presetIcon
+
+      specify "can parse preset icons" $ \Preset{..} -> do
+        let ihdrChunk = head presetIcon
             iendChunk = last presetIcon
             width     = decode (BL.take 4 $ BL.drop 4 ihdrChunk) :: Word32
             height    = decode (BL.take 4 $ BL.drop 8 ihdrChunk) :: Word32
@@ -52,9 +49,9 @@ specPreset = describe "Preset" $ do
         height `shouldBe` 200
 
         iendChunk `shouldBe` "IEND"
-      specify "can lookup resources by MD5 checksum" $ \content -> do
+
+      specify "can lookup resources by MD5 checksum" $ \preset -> do
         let md5sum = "b877c93efe4540891304ae3662e9ce58"
-            preset = decode content
             brush  = fromJust $ lookupResourceByMD5 md5sum preset
 
         resourceName brush `shouldBe` "egg"

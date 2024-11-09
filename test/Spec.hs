c74a88791142449b622ab605843b705dcdcc9e25
Quytelda Kahja <quytelda@tamalin.org>
2024-11-08 23:46:14 -0800

    Fix overindented do-block body.

--- test/Spec.hs
+++ test/Spec.hs
@@ -61,73 +61,73 @@ spec_App = do
 
 spec_Preset :: Spec
 spec_Preset = describe "Preset" $ do
-    before (decode <$> BL.readFile path_basicShapeGrainy) $ do
-      specify "can parse preset version" $ \Preset{..} ->
-        presetVersion `shouldBe` "5.0"
+  before (decode <$> BL.readFile path_basicShapeGrainy) $ do
+    specify "can parse preset version" $ \Preset{..} ->
+      presetVersion `shouldBe` "5.0"
 
-      specify "can parse preset names" $ \Preset{..} ->
-        presetName `shouldBe` "e) Basic Shape Grainy"
+    specify "can parse preset names" $ \Preset{..} ->
+      presetName `shouldBe` "e) Basic Shape Grainy"
 
-      specify "can parse preset paintopid" $ \Preset{..} ->
-        presetPaintop `shouldBe` "paintbrush"
+    specify "can parse preset paintopid" $ \Preset{..} ->
+      presetPaintop `shouldBe` "paintbrush"
 
-      specify "can parse preset parameters" $ \Preset{..} -> do
-        length presetParams `shouldBe` 225
+    specify "can parse preset parameters" $ \Preset{..} -> do
+      length presetParams `shouldBe` 225
 
-        Map.lookup "ColorSource/Type" presetParams `shouldBe` Just (String   "plain")
-        Map.lookup "EraserMode"       presetParams `shouldBe` Just (Internal "false")
+      Map.lookup "ColorSource/Type" presetParams `shouldBe` Just (String   "plain")
+      Map.lookup "EraserMode"       presetParams `shouldBe` Just (Internal "false")
 
-      specify "can parse embedded resources" $ \Preset{..} -> do
-        -- The example has 2 embedded resources: a brush and a pattern.
-        length embeddedResources `shouldBe` 2
+    specify "can parse embedded resources" $ \Preset{..} -> do
+      -- The example has 2 embedded resources: a brush and a pattern.
+      length embeddedResources `shouldBe` 2
 
-        embeddedResources `shouldSatisfy` Map.member "egg"
-        embeddedResources `shouldSatisfy` Map.member "hourglass.png"
+      embeddedResources `shouldSatisfy` Map.member "egg"
+      embeddedResources `shouldSatisfy` Map.member "hourglass.png"
 
-      specify "can parse preset icons" $ \Preset{..} -> do
-        let ihdrChunk = head presetIcon
-            iendChunk = last presetIcon
-            width     = decode (BL.take 4 $ BL.drop 4 ihdrChunk) :: Word32
-            height    = decode (BL.take 4 $ BL.drop 8 ihdrChunk) :: Word32
+    specify "can parse preset icons" $ \Preset{..} -> do
+      let ihdrChunk = head presetIcon
+          iendChunk = last presetIcon
+          width     = decode (BL.take 4 $ BL.drop 4 ihdrChunk) :: Word32
+          height    = decode (BL.take 4 $ BL.drop 8 ihdrChunk) :: Word32
 
-        -- The example preset contains 6 regular chunks.
-        length presetIcon `shouldBe` 6
+      -- The example preset contains 6 regular chunks.
+      length presetIcon `shouldBe` 6
 
-        BL.take 4 ihdrChunk `shouldBe` "IHDR"
-        BL.length ihdrChunk `shouldBe` pngIHDRChunkSize
-        width  `shouldBe` 200
-        height `shouldBe` 200
+      BL.take 4 ihdrChunk `shouldBe` "IHDR"
+      BL.length ihdrChunk `shouldBe` pngIHDRChunkSize
+      width  `shouldBe` 200
+      height `shouldBe` 200
 
-        iendChunk `shouldBe` "IEND"
+      iendChunk `shouldBe` "IEND"
 
-      specify "can lookup resources by MD5 checksum" $ \preset -> do
-        let md5sum = "b877c93efe4540891304ae3662e9ce58"
-            brush  = fromJust $ lookupResourceByMD5 md5sum preset
+    specify "can lookup resources by MD5 checksum" $ \preset -> do
+      let md5sum = "b877c93efe4540891304ae3662e9ce58"
+          brush  = fromJust $ lookupResourceByMD5 md5sum preset
 
-        resourceName brush `shouldBe` "egg"
-        resourceFile brush `shouldBe` "egg.png"
-        resourceType brush `shouldBe` "brushes"
-        resourceMD5  brush `shouldBe` md5sum
+      resourceName brush `shouldBe` "egg"
+      resourceFile brush `shouldBe` "egg.png"
+      resourceType brush `shouldBe` "brushes"
+      resourceMD5  brush `shouldBe` md5sum
 
-      specify "can be encoded as a lazy ByteString" $ \preset -> do
-        (decode . encode) preset `shouldBe` preset
+    specify "can be encoded as a lazy ByteString" $ \preset -> do
+      (decode . encode) preset `shouldBe` preset
 
-      specify "can change a preset icon" $ \preset -> do
-        scribble   <- BL.readFile "kpp/basic-ellipse.kpp"
-        Preset{..} <- either fail pure $ setPresetIcon scribble preset
-        let ihdrChunk = head presetIcon
-            iendChunk = last presetIcon
-            width     = decode (BL.take 4 $ BL.drop 4 ihdrChunk) :: Word32
-            height    = decode (BL.take 4 $ BL.drop 8 ihdrChunk) :: Word32
+    specify "can change a preset icon" $ \preset -> do
+      scribble   <- BL.readFile "kpp/basic-ellipse.kpp"
+      Preset{..} <- either fail pure $ setPresetIcon scribble preset
+      let ihdrChunk = head presetIcon
+          iendChunk = last presetIcon
+          width     = decode (BL.take 4 $ BL.drop 4 ihdrChunk) :: Word32
+          height    = decode (BL.take 4 $ BL.drop 8 ihdrChunk) :: Word32
 
-        length presetIcon `shouldBe` 5
+      length presetIcon `shouldBe` 5
 
-        BL.take 4 ihdrChunk `shouldBe` "IHDR"
-        BL.length ihdrChunk `shouldBe` pngIHDRChunkSize
-        width  `shouldBe` 200
-        height `shouldBe` 200
+      BL.take 4 ihdrChunk `shouldBe` "IHDR"
+      BL.length ihdrChunk `shouldBe` pngIHDRChunkSize
+      width  `shouldBe` 200
+      height `shouldBe` 200
 
-        iendChunk `shouldBe` "IEND"
+      iendChunk `shouldBe` "IEND"
 
 spec_FromArgument :: Spec
 spec_FromArgument = describe "FromArgument" $ do

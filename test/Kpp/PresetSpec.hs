2313054182bb6c96d76160ce353122bf24969129
Quytelda Kahja <quytelda@tamalin.org>
2025-04-07 00:44:22 -0700

    Rewrite Preset module tests.

--- test/Kpp/PresetSpec.hs
+++ test/Kpp/PresetSpec.hs
@@ -18,7 +18,7 @@ import           Kpp.Preset
 import           Kpp.Resource
 
 spec :: Spec
-spec = describe "Preset" $ do
+spec = do
   describe "parseSettingsXml" $ do
     it "can extract XML settings" $ do
       bytes <- BL.readFile path_basicEllipse
@@ -26,81 +26,107 @@ spec = describe "Preset" $ do
       md5sum (BL.toStrict xml) `shouldBe` "2f2715bf7fca5349608f061626e73ac2"
 
   before (decode <$> BL.readFile path_basicShapeGrainy) $ do
-    specify "can parse preset version" $ \Preset{..} ->
+    describe "getPreset" $ do
+      it "parses preset version" $ \Preset{..} ->
+        presetVersion `shouldBe` "5.0"
+
+      it "parses preset name" $ \Preset{..} ->
+        presetName `shouldBe` "e) Basic Shape Grainy"
+
+      it "parses preset paintopid" $ \Preset{..} ->
+        presetPaintop `shouldBe` "paintbrush"
+
+      it "parses preset parameters" $ \Preset{..} -> do
+        Map.size presetParams `shouldBe` 225
+
+        -- We only check a few parameters to ensure things are working.
+        -- TODO: Test a parameter of each param type.
+        Map.lookup "ColorSource/Type" presetParams `shouldBe` Just (String   "plain")
+        Map.lookup "EraserMode"       presetParams `shouldBe` Just (Internal "false")
+
+      it "parses filter configurations" $ \preset -> do
+        -- This preset has no filter.
+        presetFilter preset `shouldBe` Nothing
+
+        -- Try a preset that actually has a filter.
+        levelsKpp <- loadPreset path_levels
+        presetFilter levelsKpp `shouldBe` Just levelsFilterConfig
+
+      it "parses embedded resources" $ \Preset{..} -> do
+        Map.size embeddedResources `shouldBe` 2
+
+        let getMD5 rname = resourceMD5 <$> Map.lookup rname embeddedResources
+        getMD5 "egg"           `shouldBe` Just "b877c93efe4540891304ae3662e9ce58"
+        getMD5 "hourglass.png" `shouldBe` Just "3ca1bcf8dc1bc90b5a788d89793d2a89"
+
+      it "parses preset icon" $ \Preset{..} -> do
+        length presetIcon `shouldBe` 6
+
+        let ihdrChunk = head presetIcon
+            iendChunk = last presetIcon
+            width     = decode (BL.take 4 $ BL.drop 4 ihdrChunk) :: Word32
+            height    = decode (BL.take 4 $ BL.drop 8 ihdrChunk) :: Word32
+
+        BL.take 4 ihdrChunk `shouldBe` "IHDR"
+        BL.length ihdrChunk `shouldBe` pngIHDRChunkSize
+        width  `shouldBe` 200
+        height `shouldBe` 200
+
+        iendChunk `shouldBe` "IEND"
+
+    -- End: getPreset
+
+    describe "putPreset" $ do
+      it "encodes a preset as a ByteString" $ \preset -> do
+        (decode . encode) preset `shouldBe` preset
+
+      it "encodes filter configs correctly" $ \_ -> do
+        let file = testDir </> "render_filter.kpp"
+        loadPreset path_levels >>= savePreset file
+        renderFilterKpp <- loadPreset file
+        presetFilter renderFilterKpp `shouldBe` Just levelsFilterConfig
+
+    describe "lookupResourceByMD5" $ do
+      it "finds a resource by MD5 checksum" $ \preset -> do
+        let csum  = "b877c93efe4540891304ae3662e9ce58"
+            brush = fromJust $ lookupResourceByMD5 csum preset
+
+        resourceName brush `shouldBe` "egg"
+        resourceFile brush `shouldBe` "egg.png"
+        resourceType brush `shouldBe` "brushes"
+        resourceMD5  brush `shouldBe` csum
+
+    describe "setPresetIcon" $ do
+      it "changes a preset icon" $ \preset -> do
+        ellipse    <- BL.readFile "kpp/basic-ellipse.kpp"
+        Preset{..} <- either fail pure $ setPresetIcon ellipse preset
+
+        length presetIcon `shouldBe` 5
+
+        let ihdrChunk = head presetIcon
+            iendChunk = last presetIcon
+            width     = decode (BL.take 4 $ BL.drop 4 ihdrChunk) :: Word32
+            height    = decode (BL.take 4 $ BL.drop 8 ihdrChunk) :: Word32
+
+        BL.take 4 ihdrChunk `shouldBe` "IHDR"
+        BL.length ihdrChunk `shouldBe` pngIHDRChunkSize
+        width  `shouldBe` 200
+        height `shouldBe` 200
+
+        iendChunk `shouldBe` "IEND"
+
+    describe "presetIconDimensions" $ do
+      it "returns preset icon dimensions" $ \preset ->
+        presetIconDimensions preset `shouldBe` (200, 200)
+
+  describe "loadPreset" $ do
+    it "reads presets from file" $ do
+      Preset{..} <- loadPreset path_basicShapeGrainy
+
       presetVersion `shouldBe` "5.0"
-
-    specify "can parse preset names" $ \Preset{..} ->
-      presetName `shouldBe` "e) Basic Shape Grainy"
-
-    specify "can parse preset paintopid" $ \Preset{..} ->
+      presetName    `shouldBe` "e) Basic Shape Grainy"
       presetPaintop `shouldBe` "paintbrush"
+      presetFilter  `shouldBe` Nothing
 
-    specify "can parse preset parameters" $ \Preset{..} -> do
       length presetParams `shouldBe` 225
-
-      Map.lookup "ColorSource/Type" presetParams `shouldBe` Just (String   "plain")
-      Map.lookup "EraserMode"       presetParams `shouldBe` Just (Internal "false")
-
-    specify "can parse filter settings" $ \preset -> do
-      presetFilter preset `shouldBe` Nothing
-
-      levelsKpp <- loadPreset path_levels
-      presetFilter levelsKpp `shouldBe` Just levelsFilterConfig
-
-    specify "can render filter settings" $ \_ -> do
-      let file = testDir </> "render_filter.kpp"
-      loadPreset path_levels >>= savePreset file
-      renderFilterKpp <- loadPreset file
-      presetFilter renderFilterKpp `shouldBe` Just levelsFilterConfig
-
-    specify "can parse embedded resources" $ \Preset{..} -> do
-      -- The example has 2 embedded resources: a brush and a pattern.
-      length embeddedResources `shouldBe` 2
-
-      embeddedResources `shouldSatisfy` Map.member "egg"
-      embeddedResources `shouldSatisfy` Map.member "hourglass.png"
-
-    specify "can parse preset icons" $ \Preset{..} -> do
-      let ihdrChunk = head presetIcon
-          iendChunk = last presetIcon
-          width     = decode (BL.take 4 $ BL.drop 4 ihdrChunk) :: Word32
-          height    = decode (BL.take 4 $ BL.drop 8 ihdrChunk) :: Word32
-
-      -- The example preset contains 6 regular chunks.
-      length presetIcon `shouldBe` 6
-
-      BL.take 4 ihdrChunk `shouldBe` "IHDR"
-      BL.length ihdrChunk `shouldBe` pngIHDRChunkSize
-      width  `shouldBe` 200
-      height `shouldBe` 200
-
-      iendChunk `shouldBe` "IEND"
-
-    specify "can lookup resources by MD5 checksum" $ \preset -> do
-      let csum  = "b877c93efe4540891304ae3662e9ce58"
-          brush = fromJust $ lookupResourceByMD5 csum preset
-
-      resourceName brush `shouldBe` "egg"
-      resourceFile brush `shouldBe` "egg.png"
-      resourceType brush `shouldBe` "brushes"
-      resourceMD5  brush `shouldBe` csum
-
-    specify "can be encoded as a lazy ByteString" $ \preset -> do
-      (decode . encode) preset `shouldBe` preset
-
-    specify "can change a preset icon" $ \preset -> do
-      scribble   <- BL.readFile "kpp/basic-ellipse.kpp"
-      Preset{..} <- either fail pure $ setPresetIcon scribble preset
-      let ihdrChunk = head presetIcon
-          iendChunk = last presetIcon
-          width     = decode (BL.take 4 $ BL.drop 4 ihdrChunk) :: Word32
-          height    = decode (BL.take 4 $ BL.drop 8 ihdrChunk) :: Word32
-
-      length presetIcon `shouldBe` 5
-
-      BL.take 4 ihdrChunk `shouldBe` "IHDR"
-      BL.length ihdrChunk `shouldBe` pngIHDRChunkSize
-      width  `shouldBe` 200
-      height `shouldBe` 200
-
-      iendChunk `shouldBe` "IEND"
+      length presetIcon   `shouldBe` 6
